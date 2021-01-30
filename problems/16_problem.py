19c8ef112913162ac1f86b546e65d2e733b79c30
Dinmukhamet <igissinov_d@auca.kg>
2021-01-30 19:56:39 +0600

    add new problems

---
+++ problems/16_problem.py
@@ -0,0 +1,11 @@
+"""
+Given a positive integer n, calculate the following sum: 
+
+n + n/2 + n/4 + n/8 + ...
+
+All elements of the sum are the results of integer division.
+
+Example
+[In] 25
+[Out] 47
+"""
